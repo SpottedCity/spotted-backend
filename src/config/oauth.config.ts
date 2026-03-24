0ebf93f7cdedef05215cd6c1d440b38dfbc0183a
Rastaxd <149626831+Rastaxd@users.noreply.github.com>
2026-03-24 15:58:06 +0100

    Migration to Supabase witj JIT provisioning

--- src/config/oauth.config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('oauth', () => ({
-  google: {
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  },
-}));
